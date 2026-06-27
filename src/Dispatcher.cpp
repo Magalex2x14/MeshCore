1c809d9ea3bfd6533dbd574bcc74a30e3b7e328b
Aleksey Makarenko <magalex28@gmail.com>
2026-06-28 00:50:53 +0300

    8 sec HW CAD timeout

--- src/Dispatcher.cpp
+++ src/Dispatcher.cpp
@@ -60,7 +60,7 @@ uint32_t Dispatcher::getCADFailRetryDelay() const {
   return 200;
 }
 uint32_t Dispatcher::getCADFailMaxDuration() const {
-  return 4000;   // 4 seconds
+  return 8000;   // 8 seconds
 }
 
 void Dispatcher::loop() {
