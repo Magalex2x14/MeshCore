b0e913b6cf9a1602773f7088046f5d99e7933fa5
Aleksey Makarenko <magalex28@gmail.com>
2026-08-06 00:13:16 +0300

    getCADFailMaxDuration 16 secs

--- src/Dispatcher.cpp
+++ src/Dispatcher.cpp
@@ -60,7 +60,7 @@ uint32_t Dispatcher::getCADFailRetryDelay() const {
   return 200;
 }
 uint32_t Dispatcher::getCADFailMaxDuration() const {
-  return 4000;   // 4 seconds
+  return 16000;   // 16 seconds
 }
 
 void Dispatcher::loop() {
